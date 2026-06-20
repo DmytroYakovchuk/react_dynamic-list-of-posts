b11bb71216c7e4c9f3fa4e05ee60e4c1d2e1ba61
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2026-06-20 11:12:25 +0300

    with DOM

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { User } from '../types/User';
 import classNames from 'classnames';
 
@@ -14,11 +14,30 @@ export const UserSelector: React.FC<Props> = ({
   onSelect,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   const selectedUser = users.find(user => user.id === selectedUserId);
 
   return (
     <div
+      ref={dropdownRef}
       data-cy="UserSelector"
       className={classNames('dropdown', {
         'is-active': isOpen,
@@ -40,7 +59,7 @@ export const UserSelector: React.FC<Props> = ({
         </button>
       </div>
 
-      {isOpen && (
+      <>
         <div className="dropdown-menu" id="dropdown-menu" role="menu">
           <div className="dropdown-content">
             {users.map(user => (
@@ -61,7 +80,7 @@ export const UserSelector: React.FC<Props> = ({
             ))}
           </div>
         </div>
-      )}
+      </>
     </div>
   );
 };
